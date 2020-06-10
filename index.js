284558aeb571974a2c75fc26240ef714404e026b
anguyen01 <adam.nguyen@verizonmedia.com>
2020-06-10 12:44:30 -0700

    add dyno wakeup

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ const express = require('express')
 const cors = require('cors')
 const getData = require('./src/apis/youtube')
 const app = express()
-const port = process.env.PORT;
-const rateLimit = require("express-rate-limit");
+const port = process.env.PORT
+const homeURL = process.env.HOME_URL
+const rateLimit = require('express-rate-limit')
+const wakeUpDyno = require('./src/apis/wakeup')
  
-app.set('trust proxy', 1);
+app.set('trust proxy', 1)
  
 const limiter = rateLimit({
   windowMs: 30 * 60 * 1000,
   max: 80
-});
+})
  
-app.use(limiter);
+app.use(limiter)
+
+app.get('/', cors(), async (req, res, next) => {
+  res.send('Success!')
+})
 
 app.get('/search/:q', cors(), async (req, res, next) => {
   const response = await getData(req.params)
   res.send(JSON.stringify(response.data))
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => {
+  wakeUpDyno(homeURL)
+  console.log(`Example app listening at http://localhost:${port}`)
+})
