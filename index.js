78cde25cadbd631348326d9a9610dd626944ca8b
anguyen01 <adam.nguyen@verizonmedia.com>
2020-06-09 22:08:20 -0700

    add rate limiter

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const cors = require('cors')
 const getData = require('./src/apis/youtube')
 const app = express()
 const port = process.env.PORT;
+const rateLimit = require("express-rate-limit");
+ 
+app.set('trust proxy', 1);
+ 
+const limiter = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  max: 80
+});
+ 
+app.use(limiter);
 
 app.get('/search/:q', cors(), async (req, res, next) => {
   const response = await getData(req.params)
